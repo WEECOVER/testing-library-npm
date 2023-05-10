7cfbd81e4dff10b9184be755c965123cee0ffc65
CrisWeecover <cris.garcia@weecover.com>
2023-05-10 17:47:39 +0200

    VERSION 5

--- rollup.config.mjs
+++ rollup.config.mjs
@@ -1,39 +1,39 @@
 import resolve from "@rollup/plugin-node-resolve";
+import babel from "@rollup/plugin-babel";
+import peerDepsExternal from "rollup-plugin-peer-deps-external";
 import commonjs from "@rollup/plugin-commonjs";
-import typescript from "@rollup/plugin-typescript";
-import dts from "rollup-plugin-dts";
+import sass from "node-sass";
 import postcss from "rollup-plugin-postcss";
-
 import packageJson from "./package.json" assert { type: "json" };
 
-export default [
-  {
-    input: "src/index.ts",
-    output: [
-      {
-        file: packageJson.main,
-        format: "cjs",
-        sourcemap: true,
-      },
-      {
-        file: packageJson.module,
-        format: "cjs",
-        sourcemap: true,
-      },
-    ],
-    plugins: [
-      resolve(),
-      commonjs(),
-      typescript({ tsconfig: "./tsconfig.json" }),
-      postcss({
-        plugins: [],
-      }),
-    ],
-  },
-  {
-    input: "dist/esm/types/index.d.ts",
-    output: [{ file: "dist/index.d.ts", format: "esm" }],
-    plugins: [dts()],
-    external: [/\.css$/],
-  },
-];
+export default {
+  input: "src/index.js", // All of your library files will be named exports from here
+  output: [
+    {
+      // This is an easy way to keep your `main` in sync between rollup & the package
+      file: packageJson.main,
+      format: "cjs",
+      sourcemap: true,
+    },
+  ],
+  plugins: [
+    // This prevents needing an additional `external` prop in this config file by automaticall excluding peer dependencies
+    peerDepsExternal(),
+    // Convert CommonJS modules to ES6
+    commonjs({
+      include: "node_modules/**",
+      // This was required to fix some random errors while building
+    }),
+    // "...locates modules using the Node resolution algorithm"
+    resolve(),
+    // Do Babel transpilation
+    babel({
+      exclude: "node_modules/**",
+      babelHelpers: "bundled",
+    }),
+    // Does a number of things; Compiles sass, run autoprefixer, creates a sourcemap, and saves a .css file
+    postcss({
+      plugins: [],
+    }),
+  ],
+};
--- src/components/Button/Button.js
+++ src/components/Button/Button.js
@@ -1,11 +1,7 @@
 import React from "react";
 import "./Button.css";
 
-export interface ButtonProps {
-  label: string;
-}
-
-const Button = (props: ButtonProps) => {
+const Button = (props) => {
   return <button className="bespoke-button">{props.label}</button>;
 };
 
